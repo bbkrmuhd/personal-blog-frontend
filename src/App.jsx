ce2e090fb05f7ebf92126a085820ef644d1f2ead
Abubakar Muhammad Sadiq <bbkrmuhdsaddiq@gmail.com>
2022-09-24 23:39:05 +0100

    refactor files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css'
-import LoginPage from './pages/LoginPage'
+import {Home, About} from './pages'
+
 
 export default function App() {
   return (
     <>
-    <LoginPage />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </>
      
   )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import {NavBar} from '../components'
+
+const Home = () => {
+
+  return (
+    <>   
+    <NavBar /> 
+    </>
+    
+  )
+}
+
+export default Home

2d56f1bf95c2dfce586b2456cba803274b42272c
Abubakar Muhammad Sadiq <bbkrmuhdsaddiq@gmail.com>
2022-10-03 00:52:48 +0100

    add responsive to Foryou section

--- src/components/ForYou.jsx
+++ src/components/ForYou.jsx
@@ -3,50 +3,54 @@ import Button from './Button'
 import {RiHeartLine} from 'react-icons/ri'
 import {FaRegComments} from 'react-icons/fa'
 import {MdDateRange} from 'react-icons/md'
+import Fetch from '../hooks/Fetch'
 import H1 from './H1'
+import ToDate from './ToDate'
+import { Link } from 'react-router-dom'
 
 
 
 
- const ForYouPost = () => {
+ const ForYouPost = ({ post: { author, title , created_on, body, comments_count, likes_count, slug } }) => {
+
+
+
   return (
     <div className='shadow-md rounded-lg'>
-    <div className='h-40 rounded-lg bg-cover' style={{'backgroundImage': 'url(https://images.pexels.com/photos/4145355/pexels-photo-4145355.jpeg?auto=compress&cs=tinysrgb&w=800)'}}>
+       <Link to={`/post/detail/${slug}`} ><div className='h-40 rounded-lg bg-cover transition-all hover:scale-105 hover:translate-y-1 ' style={{'backgroundImage': 'url(https://images.pexels.com/photos/4145355/pexels-photo-4145355.jpeg?auto=compress&cs=tinysrgb&w=800)'}}>
 
     </div>
+    </Link>
     <div className='flex flex-col my-5 gap-2 px-2'>
         <div>
-            <h2 className='font-bold text-justify text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
+         <Link to={`/post/detail/${slug}`} > <h2 className='font-bold text-justify text-gray-900 hover:text-cyan-700' >{title}</h2></Link> 
         </div>
         <div>
-            <p className='text-[10px] text-justify text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis sapiente quia. Dignissimos numquam facere itaque ill....</p>
+            <p className='text-[10px] text-justify text-gray-700'>{body}</p>
 
         </div>
         <div></div>
         <div className='flex items-center justify-between mt-3'>
             <div className='flex items-center gap-1 text-gray-500 text-xs'>
-            <img className='w-10' src="https://avatars.githubusercontent.com/u/68012668?v=4" alt="" />
+            <img className='w-10 hover:scale-105' src="https://avatars.githubusercontent.com/u/68012668?v=4" alt="" />
             <div className='flex flex-col gap-1'>
                 <div className='flex items-center gap-1'>
-                <p>by <span className='font-bold text-gray-900'>Ameer</span></p>-
-            <p>5 min read</p>
+                <p>by <span className='font-bold text-gray-900'>{author}</span></p>-
+                 <p>5 min read</p>
                 </div>  
-                <div className='text-xs flex items-center gap-1'><span><MdDateRange/></span><p>Sep 3 2022</p></div>
+                <div className='text-xs flex items-center gap-1'><span><MdDateRange/></span><ToDate date={created_on} /></div>
                 </div>
-
-           
-          
 
             </div>
             <div className='flex items-center gap-2'>
-                <p className='flex items-center gap-2'>
+                <p className='flex items-center gap-2 hover:text-red-500'>
                 <RiHeartLine />
-                <span className='text-xs'>11</span>
+                <span className='text-xs'>{likes_count}</span>
 
                 </p>
-                <p className='flex items-center gap-2 '>
+                <p className='flex items-center gap-2 hover:text-cyan-500'>
                 <FaRegComments />
-                <span className='text-xs'>50</span>
+                <span className='text-xs'>{comments_count}</span>
                 </p>
             </div>
             
@@ -73,9 +77,14 @@ const ForYou = () => {
         </div>
         <div className='grid grid-cols-1 md:grid-cols-2  gap-4 sm:gap-6 my-5'>
 
-        {topics.map(topic => (
-              <ForYouPost />
-            ))}   
+        <Fetch
+            url="posts/list"
+            renderSuccess={({ data: { posts } }) => (
+            posts.map(post => (
+        <ForYouPost key={post.id} post={post} />
+        ))
+        )}
+        />
          
 
         </div>

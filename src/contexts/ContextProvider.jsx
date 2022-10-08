53c1e4e2e291c4f3a9f9e4bfc5bfe0173a3acabf
Abubakar Muhammad Sadiq <bbkrmuhdsaddiq@gmail.com>
2022-10-08 22:32:43 +0100

    add AuthContext provider

---
+++ src/contexts/ContextProvider.jsx
@@ -0,0 +1,47 @@
+import { useState, createContext, useEffect } from "react";
+import { LocalStorageService } from "../services/LocalStorageService";
+
+export const AuthContext = createContext()
+
+
+export const AuthContextProvider= ({children}) => {
+    const [currentUser, setCurrentUser] = useState(LocalStorageService.loadJSON("user") || null)
+
+    const login = async (inputs) => {
+        const res = await fetch("http://127.0.0.1:5000/api/v1/login", {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(inputs)})
+            .then((result) => result.json())
+            .then((data) => {
+
+             setCurrentUser(data)
+           })  
+    }
+
+    const logout = async (inputs) => {
+        const res = await fetch("http://127.0.0.1:5000/api/v1/logout ", {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(inputs)})
+           .then((result) => result.json())
+           .then((data) => {
+             setCurrentUser(null)
+           })
+           LocalStorageService.deleteJSON("user")
+    }
+
+
+    useEffect(() => {
+
+        LocalStorageService.saveJSON("user", currentUser)
+    
+    }, [currentUser])
+
+
+    return (<AuthContext.Provider value={{currentUser, login, logout}}>
+                {children}
+            </AuthContext.Provider>)
+    
+
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
-import React, {useState, } from 'react'
+import React, {useState, useEffect, useRef, useContext } from 'react'
 import { LocalStorageService } from '../services/LocalStorageService'
 import {useNavigate} from 'react-router-dom'
+import { AuthContext } from '../contexts/ContextProvider'
 
 
 
@@ -8,35 +9,41 @@ function LoginPage() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [loading, setLoading] = useState(false)
+ 
 
 
   const navigate = useNavigate()
+  const inputFocus = useRef(null)
+  const {login, currentUser} = useContext(AuthContext)
 
 
-  console.log(sessionStorage.getItem("csrf_token"))
-  localStorage.setItem("hello", "hello")
-  LocalStorageService.saveJSON("user_name", "sadiqcodes")
+  useEffect(() => {
+    inputFocus.current.focus()
+  }, [])
 
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
       e.preventDefault()
-      const login = {email, password}
+      const loginInputs = {email, password}
       setLoading(true)
-      fetch("http://127.0.0.1:5000/api/v1/login", {
-             method: 'POST',
-             headers: {"Content-Type": "application/json"},
-             body: JSON.stringify(login)
-            }).then((result) => result.json()
-            ).then((data) => {
-              console.log(data)
-              LocalStorageService.saveJSON("access_token", data.access_token)
-              setLoading(false)
-              navigate('/sirri')
-              console.log("login successfully")
-            })
+      try {
+        await login(loginInputs)
+        setLoading(false)
+        if (currentUser.is_admin){
+          navigate("/sirri")
+
+        } else{
+          navigate("/")
+        }
+        
+
+      } catch (error) {
+        console.log(error)
+      }
       setEmail("")
       setPassword("")
   }
+
 
   return (  
     <div className='container mx-auto h-screen '>
@@ -51,12 +58,13 @@ function LoginPage() {
           <form className='my-8 sm:my-12' onSubmit={handleLogin}>
             <div className='flex flex-col space-y-2 my-2'>  
                 <label className='text-sm sm:text-base' htmlFor="email">EMAIL:</label>
-                <input className='p-1 sm:p-2 border-2 rounded-md border-gray-300 bg-gray-50 text-gray-900 focus:border-cyan-500 outline-none peer-focus:outline-none focus:ring-cyan-300' type="email" value={email} onChange ={(e) => setEmail(e.target.value)} />
+                <input ref={inputFocus} className='p-1 sm:p-2 border-2 rounded-md border-gray-300 bg-gray-50 text-gray-900 focus:border-cyan-500 outline-none peer-focus:outline-none focus:ring-cyan-300' type="email" value={email} onChange ={(e) => setEmail(e.target.value)} />
             </div>
             <div className='flex flex-col space-y-2 my-2'>  
                 <label className='text-sm sm:text-base' htmlFor="password">PASSWORD:</label>
                 <input className='p-1 sm:p-2 border-2 rounded-md border-gray-300 bg-gray-50 text-gray-900 focus:border-cyan-500 outline-none peer-focus:outline-none focus:ring-cyan-300' type="password"  value={password} onChange = {(e) => setPassword(e.target.value)}/>
             </div>
+            {/* {error &&   <pre>{JSON.stringify(error, null, 2)}</pre>} */}
             {!loading &&  <button className='py-2 px-4 sm:px-6 my-5 bg-cyan-500 text-white rounded-md text-sm sm:text-base' type='submit' >Login</button>}
             {loading && <button className='py-2 px-4 sm:px-6 my-5 bg-cyan-500 text-white rounded-md text-sm sm:text-base' type='submit' disabled >Login...</button>}
          

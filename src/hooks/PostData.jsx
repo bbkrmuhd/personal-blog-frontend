9198fff84d7010db0000b9a2b6fa0f49c62559d0
Abubakar Muhammad Sadiq <bbkrmuhdsaddiq@gmail.com>
2023-02-17 22:47:03 +0100

    feat: implement adding comments using useMutation

--- src/hooks/PostData.jsx
+++ src/hooks/PostData.jsx
@@ -1,45 +1,24 @@
-import React, {useState, useEffect} from 'react'
-import { LocalStorageService } from '../services/LocalStorageService'
+import axios from "axios"
+import {useMutation, useQueryClient} from 'react-query'
+import {config} from '../config/environment'
+
+const base_url = config.base_url
+
+const addCommentData = ({postSlug, formData: comment}) => { 
+  return axios.post(`${base_url}comment/create/${postSlug}`, comment)
+}
 
 
-
-  export const usePost = async (url, formData) => {
-    const [result, setResult] = useState(null)
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
-
-    let token = LocalStorageService.loadJSON("access_token")
-    console.log("token", token)
-
-    useEffect(() => {
-      if (!url && data) return;
-      const response = async (url) =>{
-        await fetch(url, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-            'Authorization': `Bearer ${token}`,
-          },
-
-          body: JSON.stringify(formData)
-        
-        })
-
-        return await response.json()
-
-      }
-      
-
-      setResult(response)
-      setLoading(false)
-      
-      
-    }, [url])
-    console.log(error)
-    return {result, loading, error};
-  }
-
-
-  
-
-
+export const useAddCommentData = () => {
+  const queryClient = useQueryClient()
+  return useMutation((data) => addCommentData(data),{
+    onSuccess: (data) => {
+      queryClient.setQueryData(['post-comments', data?.data.comment.post_slug], (oldData) => {
+        console.log(oldData?.data.comments)
+        return {
+          ...oldData, data: [...oldData?.data.comments, data.data.comment]
+        }
+      })
+    }
+  })
+}
